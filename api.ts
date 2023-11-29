3c4a3affa3852f877eaccd89de17303228e16c64
Primathon <aastha.pandey@primathon.in>
2023-11-29 12:20:06 +0530

    Added API base URL acc to Naming convention

--- api.ts
+++ api.ts
@@ -1,15 +1,15 @@
 import { ITask } from "./types/tasks";
 
-const baseUrl = 'http://localhost:3001';
+const API_BASE_URL = 'http://localhost:3001';
 
 export const getAllTodos = async (): Promise<ITask[]> => {
-    const res = await fetch(`${baseUrl}/tasks`, {cache: 'no-store'});
+    const res = await fetch(`${API_BASE_URL}/tasks`, {cache: 'no-store'});
     const todos = await res.json();
     return todos;
 }
 
 export const addTodo = async (todo: ITask): Promise<ITask> => {
-    const res = await fetch(`${baseUrl}/tasks`, {
+    const res = await fetch(`${API_BASE_URL}/tasks`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -21,7 +21,7 @@ export const addTodo = async (todo: ITask): Promise<ITask> => {
 }
 
 export const editTodo = async (todo: ITask): Promise<ITask> => {
-    const res = await fetch(`${baseUrl}/tasks/${todo.id}`, {
+    const res = await fetch(`${API_BASE_URL}/tasks/${todo.id}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
@@ -33,7 +33,7 @@ export const editTodo = async (todo: ITask): Promise<ITask> => {
 }
 
 export const deleteTodo = async (id: string): Promise<void> => {
-    const res = await fetch(`${baseUrl}/tasks/${id}`, {
+    const res = await fetch(`${API_BASE_URL}/tasks/${id}`, {
         method: 'DELETE'
     })
 }
